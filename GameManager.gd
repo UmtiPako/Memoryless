69559b3da889937a37fc7d450396b4fa869e9efe
DENIZSEK <161316112+DENIZSEKER99@users.noreply.github.com>
2025-05-31 13:35:55 +0300

    heart filan

--- GameManager.gd
+++ GameManager.gd
@@ -13,33 +13,5 @@ var scenes: Array[PackedScene] = [scene1, scene2]
 var player_health: int
 
 
-func _update_canvas_hearths():
-	if player_health == 4:
-		heart.modulate.a = 1
-		heart_2.modulate.a = 1
-		heart_3.modulate.a = 1
-		heart_4.modulate.a = 1
-	if player_health == 3:
-		heart.modulate.a = 1
-		heart_2.modulate.a = 1
-		heart_3.modulate.a = 1
-		heart_4.modulate.a = 0.3
-	if player_health == 2:
-		heart.modulate.a = 1
-		heart_2.modulate.a = 1
-		heart_3.modulate.a = 0.3
-		heart_4.modulate.a = 0.3
-	if player_health == 1:
-		heart.modulate.a = 1
-		heart_2.modulate.a = 0.3
-		heart_3.modulate.a = 0.3
-		heart_4.modulate.a = 0.3
-	if player_health == 0:
-		heart.modulate.a = 0.3
-		heart_2.modulate.a = 0.3
-		heart_3.modulate.a = 0.3
-		heart_4.modulate.a = 0.3
-	pass
-
 func select_random_room() -> PackedScene:
 	return scenes[randf_range(0, len(scenes))]
