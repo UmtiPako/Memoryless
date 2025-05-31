d626a3cfcb901f4f65e9ce80d0a073ced9894f09
Umut Paklacı <71903884+UmtiPako@users.noreply.github.com>
2025-05-31 10:36:21 +0300

    puş

--- People/Enemies/enemy.gd
+++ People/Enemies/enemy.gd
@@ -48,6 +48,7 @@ func _ready():
 
 
 func _physics_process(_delta: float):
+
 	if not player or not is_instance_valid(player):
 		# Player might have been removed (e.g. died)
 		# Consider what enemies should do here (e.g. go idle, despawn)
