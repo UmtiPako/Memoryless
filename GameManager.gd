8dba1986330d63d1f8e90489e466d5561e4963f1
Umut Paklacı <71903884+UmtiPako@users.noreply.github.com>
2025-05-31 03:04:47 +0300

    Random oda değişimi

--- GameManager.gd
+++ GameManager.gd
@@ -4,3 +4,6 @@ var scene1: PackedScene = preload("res://Scenes/test_level.tscn")
 var scene2: PackedScene = preload("res://Scenes/test_level_2.tscn")
 
 var scenes: Array[PackedScene] = [scene1, scene2]
+
+func select_random_room() -> PackedScene:
+	return scenes[randf_range(0, len(scenes))]
--- Props/Interactive/Door/door.gd
+++ Props/Interactive/Door/door.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-var scene: PackedScene = preload("res://Scenes/test_level_2.tscn")
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -14,4 +12,4 @@ func _process(delta: float) -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
-		AnimationManager.transition_to_scene(scene)
+		AnimationManager.transition_to_scene(GameManager.select_random_room())
