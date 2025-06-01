2b80c40583dbb6a7be793774b7bb40b6834da7b7
DENIZSEK <161316112+DENIZSEKER99@users.noreply.github.com>
2025-06-01 11:22:30 +0300

    final scene fix

---
+++ UI/endings/slide_up_ending.gd
@@ -0,0 +1,18 @@
+extends Sprite2D
+@onready var timer: Timer = $"../Timer"
+var over : bool = false
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		GlobalTimer.is_game_started = true
+		get_tree().change_scene_to_file("res://Scenes/enterance_menu.tscn")
+		
+	
+	if not over:
+		self.position.y = self.position.y -80*delta
+	
+func _ready() -> void:
+	timer.start()
+
+
+func _on_timer_timeout() -> void:
+	over = true
