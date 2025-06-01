8ddb1b2c2d863f2bca7bfcd192d1da0cb848643c
DENIZSEK <161316112+DENIZSEKER99@users.noreply.github.com>
2025-06-01 03:44:57 +0300

    menu

---
+++ Scenes/start.gd
@@ -0,0 +1,29 @@
+extends Button
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_down() -> void:
+	animation_player.play("pop")
+	await animation_player.animation_finished
+	GlobalTimer.is_game_started = true
+	get_tree().change_scene_to_file("res://Scenes/Places/dışarı/sokak_1.tscn")
+	pass # Replace with function body.
+
+
+func _on_button_button_down() -> void:
+	animation_player.play("pop")
+	await animation_player.animation_finished
+	GlobalTimer.is_game_started = true
+	get_tree().change_scene_to_file("res://Scenes/Places/dışarı/sokak_1.tscn")
+	pass # Replace with function body.
